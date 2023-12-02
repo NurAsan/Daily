9e3fc32a1202f6fb0d563aff31debfd96b16779a
nurasan <210103229@stu.sdu.edu.kz>
2023-12-02 09:44:05 +0600

    final version in one function

--- app/src/main/java/com/example/daily/MainActivity.kt
+++ app/src/main/java/com/example/daily/MainActivity.kt
@@ -3,20 +3,26 @@ package com.example.daily
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.shapes
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -26,9 +32,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.daily.data.Recep
 import com.example.daily.data.receps
 import com.example.daily.ui.theme.DailyTheme
@@ -42,7 +52,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    RecipeApp()
                 }
             }
         }
@@ -52,7 +62,7 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun WoofApp() {
+fun RecipeApp() {
     Scaffold(
         topBar = {
             TopBar()
@@ -83,11 +93,53 @@ fun RecepItem(
                 .fillMaxWidth()
                 .padding(dimensionResource(R.dimen.padding_small))
         ) {
-            Spacer(modifier = Modifier.weight(1f))
+            RecipeInformation(recipes.name, recipes.hobbies,recipes.imageResourceId )
         }
 
     }
 }
+
+/*
+@Composable
+fun RecipeImage(
+    @DrawableRes recipeImg : Int,
+    modifier: Modifier = Modifier
+){
+    Image(painter = painterResource(recipeImg),
+        contentDescription = null,
+        modifier = Modifier
+            .size(200.dp)
+            .clip(shapes.medium)
+    )
+}*/
+
+@Composable
+fun RecipeInformation(
+    @StringRes recipeName: Int,
+    @StringRes recipeInfo : Int,
+    @DrawableRes recipeImg : Int,
+    modifier: Modifier = Modifier
+){
+    Column(modifier = Modifier) {
+        Text(
+            text = stringResource(recipeName),
+            style = MaterialTheme.typography.displayMedium,
+        )
+        Image(painter = painterResource(recipeImg),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxSize()
+                .clip(shapes.small)
+        )
+        Text(
+            text =  stringResource(recipeInfo),
+            style = MaterialTheme.typography.labelLarge
+        )
+    }
+}
+
+
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TopBar(modifier: Modifier = Modifier){
@@ -95,11 +147,19 @@ fun TopBar(modifier: Modifier = Modifier){
         title = {
             Row() {
                 Text(
-                    text = stringResource(R.string.app_name),
+                    text = stringResource(R.string.top_bar),
                     style = MaterialTheme.typography.displayLarge
                 )
             }
         },
         modifier = modifier
     )
+}
+
+@Preview
+@Composable
+fun WoofPreview() {
+    DailyTheme(darkTheme = false) {
+        RecipeApp()
+    }
 }
